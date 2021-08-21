67900dfcfa570fbe532934d937aaac267bc6ef88
mariamb81 <mariam.busari81@gmail.com>
2021-08-21 19:08:29 +0000

    reading from DB
    Signed-off-by: mariamb81 <mariam.busari81@gmail.com>

--- public/js/form.js
+++ public/js/form.js
@@ -1,10 +1,11 @@
 var googleUserId;
+var section;
 window.onload = (event) => {
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
     document.getElementById("welcome-msg").innerHTML = `Hello, ${user.displayName}`;
       googleUserId = user;
-      
+      readfromDB(user.uid)
     } else {
       window.location = 'index.html'; 
     };
@@ -13,7 +14,6 @@ window.onload = (event) => {
 
 const handleUserInfo = () => {
     const userInput = document.getElementById("userinputform");
-    console.log(userInput.value);
     firebase.database().ref(`users/${googleUserId.uid}/profile/tags`).push({
         name: userInput.value,
   })
@@ -22,3 +22,32 @@ const handleUserInfo = () => {
   });
 };
 
+const readfromDB = (userId) => {
+  const tagsRef = firebase.database().ref(`users/${googleUserId.uid}/profile/tags`);
+  tagsRef.on('value', (snapshot) => {
+    const data = snapshot.val();
+    console.log(data)
+    renderDataAsHtml(data);
+  });
+};
+
+const renderDataAsHtml = (data) => {
+  let badges = ``;
+  for(const tagItem in data) {
+    const tag = data[tagItem];
+    badges += `<span class="badge">
+                  <button type="button" class="btn" id=${tagItem} onclick="deleteTag(this.id);">X</button
+                  >${tag.name}</span
+                >`
+  };
+  document.querySelector('#tagsapp').innerHTML = badges;
+};
+
+const deleteTag = (tagId) => {
+//    deleted
+section = "tags";
+    firebase
+      .database()
+      .ref(`users/${googleUserId.uid}/profile/${section}/${tagId}`)
+      .remove();
+};
