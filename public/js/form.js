76cc154288aa833296ad2bb0489c9778252086d9
mariamb81 <mariam.busari81@gmail.com>
2021-08-21 18:32:38 +0000

    write tags to db
    Signed-off-by: mariamb81 <mariam.busari81@gmail.com>

--- public/js/form.js
+++ public/js/form.js
@@ -1,41 +1,24 @@
+var googleUserId;
 window.onload = (event) => {
-  // Use this to retain user state between html pages.
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-      console.log('Logged in as: ' + user.displayName);
-      googleUserId = user.uid;
-      googleDisplayName = user.displayName;
-      googleEmailAddress = user.email;
-      console.log("Email: " + googleEmailAddress);
-      fillGoogleInfo();
-      getUserInfo(googleUserId);
+    document.getElementById("welcome-msg").innerHTML = `Hello, ${user.displayName}`;
+      googleUserId = user;
+      
     } else {
-      // If not logged in, navigate back to login page.
       window.location = 'index.html'; 
     };
   });
 };
 
-const fillGoogleInfo = () => {
-    theNameInput.value = googleDisplayName;
-    emailInput.value = googleEmailAddress;
-
+const handleUserInfo = () => {
+    const userInput = document.getElementById("userinputform");
+    console.log(userInput.value);
+    firebase.database().ref(`users/${googleUserId.uid}/profile/tags`).push({
+        name: userInput.value,
+  })
+  .then(() => {
+      userInput.value = "";
+  });
 };
 
-const getUserInfo = (userId) => {
-    const infoRef = firebase.database().ref(`userData/users/${userId}`);
-    infoRef.on('value', (snapshot) => {
-        const data = snapshot.val();
-        fillDataToForm(data);
-    });
-};
-
-const fillDataToForm = (data) => {
-    for(const userInfo in data){
-        const info = data[userInfo];
-        const name = user.displayName;
-        const email = user.email;
-        console.log("Name: " + name);
-        console.log("Email: " + email);
-    }
-};
