95b9c03c832c4b96466a9e4f138c2a100dc8c1f6
mariamb81 <mariam.busari81@gmail.com>
2021-08-22 04:07:04 +0000

    render data
    Signed-off-by: mariamb81 <mariam.busari81@gmail.com>

--- public/js/searchResults.js
+++ public/js/searchResults.js
@@ -31,14 +31,54 @@ const APICall = query => {
       return response.json();
     })
     .then(myjson => {
-    console.log(`Results for ${query}:`, myjson)
+    // console.log(`Results for ${query}:`, myjson)
+    
     let foodIndex =0;
     let foodItem = myjson.foods[0];
-    console.log(foodItem)
+    // console.log(foodItem)
     let foodPhoto = document.querySelector("#foodphoto");
     let foodName = document.querySelector("#foodname");
+    let foodLink = document.querySelector("#foodlink");
+    foodLink.href = `https://www.nutritionix.com/food/${query}`
     foodPhoto.src = foodItem.photo.thumb;
+
     foodName.innerHTML = ( foodItem.tags.quantity + " serving of " + foodItem.tags.item);
-    
+    parseNutritionData(foodItem);
+
   });
 };
+const parseNutritionData = (data) => {
+    const recommendedAmts = {
+    "total_carbohydrate": ((291+421)/2),
+    "dietary_fiber": (38),
+    "protein" : 38,
+    "saturated_fat" : 0,
+    "cholesterol" : 0,
+    "sodium" : 1500,
+    "sugars": 24,
+    "potassium": 3500
+    }
+    const low = 0.05;
+    const high = 0.20;
+    const prefix = "nf_";
+    container = document.getElementById("query-app");
+
+    for(const amount in recommendedAmts) {
+        var query = prefix + amount;
+        let value = data[query];
+        // console.log(value)
+        if (value ===0){
+            container += `<p>No ${amount}</p>`
+        }
+        else if (value/recommendedAmts[amount] <= low){
+            container.innerHTML += `<p>Low in ${amount}</p>`
+        }
+        else if (value/recommendedAmts >= high){
+            container.innerHTML += `<p>High in ${amount}</p>`
+        }
+        else {
+            container.innerHTML += `<p>Moderate ${amount}</p>`
+        }
+    }
+}
+
